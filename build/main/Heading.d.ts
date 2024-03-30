f373ee6f004aeed2c1e0f48c7a39429ffccaaca5
Thorne <ExistentialEnso@users.noreply.github.com>
2024-03-30 18:52:33 -0400

    New build

--- build/main/Heading.d.ts
+++ build/main/Heading.d.ts
@@ -1,6 +1,7 @@
 type HeadingProps = {
     size?: Number;
     children?: React.ReactNode;
+    className?: string;
 };
 export default function Heading(props: HeadingProps): import("react/jsx-runtime").JSX.Element;
 export {};
